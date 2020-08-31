7142c464dbd83a4f56ead83b1af122367309c153
Azeem <azeemahmed101@gmail.com>
2020-08-31 10:59:37 +0200

    fetch weather data function and get geolocation from client

--- src/index.js
+++ src/index.js
@@ -1,1 +1,21 @@
-console.log('Hello World');
+const weather = (() => {
+  async function getWeather(lat, lon) {
+    try {
+      const url = `http://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=b56e54fcfc83ea96697925d8521b8966`;
+      const response = await fetch(url, { mode: 'cors' });
+      const weatherData = await response.json();
+      console.log(weatherData);
+    } catch (err) {
+      console.log('There was an error', err);
+    }
+  }
+  function success(position) {
+    const lat = position.coords.latitude;
+    const lon = position.coords.longitude;
+    getWeather(lat, lon);
+  }
+
+  if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(success);
+  }
+})();
