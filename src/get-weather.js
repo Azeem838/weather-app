38cd3fd21d04c9c4d1bb70a69ee1c5754e20b30f
Azeem <azeemahmed101@gmail.com>
2020-09-02 10:34:24 +0200

    update url to use https

--- src/get-weather.js
+++ src/get-weather.js
@@ -4,7 +4,7 @@ import handleData from './sanatize-data';
 (() => {
   async function getWeather(lat, lon, units = 'metric') {
     try {
-      const url = `http://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=b56e54fcfc83ea96697925d8521b8966&units=${units}`;
+      const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=b56e54fcfc83ea96697925d8521b8966&units=${units}`;
       const response = await fetch(url, { mode: 'cors' });
       const result = await response.json();
       const data = handleData(result);
@@ -30,7 +30,7 @@ async function cityWeather(e) {
   const units = displayController.getUnit();
   const query = displayController.getInput();
   try {
-    const url = `http://api.openweathermap.org/data/2.5/weather?q=${query}&appid=b56e54fcfc83ea96697925d8521b8966&units=${units}`;
+    const url = `https://api.openweathermap.org/data/2.5/weather?q=${query}&appid=b56e54fcfc83ea96697925d8521b8966&units=${units}`;
     const response = await fetch(url, { mode: 'cors' });
     const result = await response.json();
     const data = handleData(result);
